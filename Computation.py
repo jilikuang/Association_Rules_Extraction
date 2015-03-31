37767f2ce55a510f8d7db91e904d3e42099dc4b4
Di Ruan <ruandi88@gmail.com>
2015-03-31 14:06:09 -0400

    add test csv

--- Computation.py
+++ Computation.py
@@ -1,1 +1,13 @@
+import csv
 
+test_file_name = 'Graduation_Outcomes_-_Citywide_-_Classes_of_2005-2011_-_Ethnicity.csv'
+
+
+def read_file(file_name):
+    with open(file_name, 'rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            print str(len(row))
+            print row
+
+read_file(test_file_name)
