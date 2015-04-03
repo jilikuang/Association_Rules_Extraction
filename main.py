ccff37f66227b3f748f2a1428cf8551641e25109
Jie-Gang Kuang <jk3735@columbia.edu>
2015-04-03 15:46:50 -0400

    Add input for main

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
+import sys
 import Computation
 
+def main(argv):
+    if argv[0] == 'test':
+        csv_filename = 'Graduation_Outcomes_-_Citywide_-_Classes_of_2005-2011_-_Ethnicity.csv'
+        min_supp = 0.5
+        min_conf = 0.5
+    else:
+        csv_filename = argv[0]
+        min_supp = float(argv[1])
+        min_conf = float(argv[2])
 
-test_file_name = 'Graduation_Outcomes_-_Citywide_-_Classes_of_2005-2011_-_Ethnicity.csv'
-min_supp = 0.5
-min_conf = 0.5
+    Computation.read_file(csv_filename)
+    Computation.categorize()
+    Computation.compute_word_collection()
+    Computation.compute_frequency()
+    Computation.apriori(min_supp, min_conf)
 
-Computation.read_file(test_file_name)
-Computation.categorize()
-Computation.compute_word_collection()
-Computation.compute_frequency()
-Computation.apriori(min_supp, min_conf)
+if __name__ == "__main__":
+    main(sys.argv[1:])
