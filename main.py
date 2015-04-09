5861e8299535d78051d1e6a96df231e5edea357e
Di Ruan <ruandi88@gmail.com>
2015-04-09 18:06:33 -0400

    add readme

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import sys
 from Computation import *
 
 
+# entry of the program
 def main(argv):
     if argv[0] == 'test':
         csv_filename = 'Graduation_Outcomes_-_Citywide_-_Classes_of_2005-2011_-_Ethnicity.csv'
-        min_supp = 0.4
+        min_supp = 0.45
         min_conf = 1.0
     else:
         csv_filename = argv[0]
@@ -20,10 +21,9 @@ def main(argv):
     display(frequent_items, association_list, min_supp, min_conf)
 
 
-# print association according to the requirement
-# each association is a list of set with length 2
+# print the high frequent item_sets and association rules according to the requirement
 def display(frequent_items, association_list, min_supp, min_conf):
-    print "== Frequent itemsets (min_sup= " + str(min_supp) + ")"
+    print "== Frequent itemsets (min_sup = " + str(min_supp) + ")"
     print frequent_items
     print ""
     print "== High-confidence association rules (min_conf = " + str(min_conf) + ")"
