5dbe26204ee2f74aab0d017900f06665bc27d4f6
Jie-Gang Kuang <jk3735@columbia.edu>
2015-04-12 12:07:27 -0400

    Add file-output for the requirement of the project

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ def main(argv):
     association_list = compute_high_conf_ass_list(frequent_items, min_conf)
 
     display(frequent_items, association_list, min_supp, min_conf)
+    output(frequent_items, association_list, min_supp, min_conf)
 
 
 def display(itemsets, rules, min_supp, min_conf):
@@ -29,12 +30,26 @@ def display(itemsets, rules, min_supp, min_conf):
     print '==Frequent itemsets (min_sup=' + str(min_supp*100.0) + '%)'
     # print itemsets
     for s in itemsets:
-        print '[' + ', '.join(s[0]) + '], ' + '{:.2f}'.format(s[1]*100.0) + '%'
+        print '[' + ', '.join(s[0]) + '], ' + '{:.4f}'.format(s[1]*100.0) + '%'
     print
     print '==High-confidence association rules (min_conf=' + str(min_conf*100.0) + '%)'
     # print rules
     for r in rules:
-        print '[' + ', '.join(r[0]) + '] => [' + ', '.join(r[1]) + '], (Conf: ' + '{:.2f}'.format(r[2]*100.0) + '%, Supp: ' + '{:.2f}'.format(r[3]*100.0) + '%)'
+        print '[' + ', '.join(r[0]) + '] => [' + ', '.join(r[1]) + '], (Conf: ' + '{:.4f}'.format(r[2]*100.0) + '%, Supp: ' + '{:.4f}'.format(r[3]*100.0) + '%)'
+
+
+def output(itemsets, rules, min_supp, min_conf):
+    out = open('output.txt', 'w')
+    out.write('==Frequent itemsets (min_sup=' + str(min_supp*100.0) + '%)\n')
+    # print itemsets
+    for s in itemsets:
+        out.write('[' + ', '.join(s[0]) + '], ' + '{:.4f}'.format(s[1]*100.0) + '%\n')
+    out.write('\n')
+    out.write('==High-confidence association rules (min_conf=' + str(min_conf*100.0) + '%)\n')
+    # print rules
+    for r in rules:
+        out.write('[' + ', '.join(r[0]) + '] => [' + ', '.join(r[1]) + '], (Conf: ' + '{:.4f}'.format(r[2]*100.0) + '%, Supp: ' + '{:.4f}'.format(r[3]*100.0) + '%)\n')
+    out.close()
 
 
 if __name__ == "__main__":
