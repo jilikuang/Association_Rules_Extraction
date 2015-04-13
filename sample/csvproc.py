a1d01a3d1ae3ea5149e6479ec3e3818c9ee187ca
Jie-Gang Kuang <jk3735@columbia.edu>
2015-04-12 23:45:56 -0400

    Add codes to discard the incomplete rows

--- sample/csvproc.py
+++ sample/csvproc.py
@@ -21,5 +21,20 @@ def process(mode, incsv, first_col):
                 for row in reader:
                     writer.writerow([first_col] + row)
 
+def csvreduce(incsv):
+    with open(incsv, 'r') as csvin:
+        with open('output.csv', 'w') as csvout:
+            reader = csv.reader(csvin)
+            writer = csv.writer(csvout)
+            first = next(reader)
+            col_num = len(first)
+            writer.writerow(first)
+            for row in reader:
+                if len(filter(str.strip, row)) == col_num:
+                    writer.writerow(row)
+
 if __name__ == '__main__':
-    process(sys.argv[1], sys.argv[2], sys.argv[3])
+    if sys.argv[1] == 'new' or sys.argv[1] == 'append':
+        process(sys.argv[1], sys.argv[2], sys.argv[3])
+    elif sys.argv[1] == 'reduce':
+        csvreduce(sys.argv[2])
