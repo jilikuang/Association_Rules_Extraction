0d970b87fcd5f2d251b2780e2eabbda7d32f6f48
Jie-Gang Kuang <jk3735@columbia.edu>
2015-04-14 19:33:17 -0400

    Modify percentage display formatting

--- main.py
+++ main.py
@@ -30,12 +30,12 @@ def display(itemsets, rules, min_supp, min_conf):
     print '==Frequent itemsets (min_sup=' + str(min_supp*100.0) + '%)'
     # print itemsets
     for s in itemsets:
-        print '[' + ', '.join(s[0]) + '], ' + '{:.4f}'.format(s[1]*100.0) + '%'
+        print '[' + ', '.join(s[0]) + '], ' + '{:.4%}'.format(s[1])
     print
     print '==High-confidence association rules (min_conf=' + str(min_conf*100.0) + '%)'
     # print rules
     for r in rules:
-        print '[' + ', '.join(r[0]) + '] => [' + ', '.join(r[1]) + '], (Conf: ' + '{:.4f}'.format(r[2]*100.0) + '%, Supp: ' + '{:.4f}'.format(r[3]*100.0) + '%)'
+        print '[' + ', '.join(r[0]) + '] => [' + ', '.join(r[1]) + '], (Conf: ' + '{:.4%}'.format(r[2]) + ', Supp: ' + '{:.4%}'.format(r[3]) + ')'
 
 
 def output(itemsets, rules, min_supp, min_conf):
@@ -43,12 +43,12 @@ def output(itemsets, rules, min_supp, min_conf):
     out.write('==Frequent itemsets (min_sup=' + str(min_supp*100.0) + '%)\n')
     # print itemsets
     for s in itemsets:
-        out.write('[' + ', '.join(s[0]) + '], ' + '{:.4f}'.format(s[1]*100.0) + '%\n')
+        out.write('[' + ', '.join(s[0]) + '], ' + '{:.4%}'.format(s[1]) + '\n')
     out.write('\n')
     out.write('==High-confidence association rules (min_conf=' + str(min_conf*100.0) + '%)\n')
     # print rules
     for r in rules:
-        out.write('[' + ', '.join(r[0]) + '] => [' + ', '.join(r[1]) + '], (Conf: ' + '{:.4f}'.format(r[2]*100.0) + '%, Supp: ' + '{:.4f}'.format(r[3]*100.0) + '%)\n')
+        out.write('[' + ', '.join(r[0]) + '] => [' + ', '.join(r[1]) + '], (Conf: ' + '{:.4%}'.format(r[2]) + ', Supp: ' + '{:.4%}'.format(r[3]) + ')\n')
     out.close()
 
 
