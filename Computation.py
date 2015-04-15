295c9113499457aa7880c2b28ae04b10829274df
Jie-Gang Kuang <jk3735@columbia.edu>
2015-04-14 21:20:16 -0400

    Extract fields from data

--- Computation.py
+++ Computation.py
@@ -7,12 +7,15 @@ cat_data_rows_list = []
 words_set = set()
 col_with_num = 3
 category_num = 5.0
+fields = []
 
 
 # read the excel file into rows
 def read_file(file_name):
     with open(file_name, 'rb') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
+        global fields
+        fields = next(reader)
         for row in reader:
             data_rows.append(row)
 
@@ -23,7 +26,7 @@ def categorize():
         mins = []
         maxs = []
         index = 0
-        for col in data_rows[1]:
+        for col in data_rows[0]:
             if index > col_with_num and col != ' n/a ':
                 mins.append(analyze(col))
                 maxs.append(analyze(col))
@@ -31,7 +34,7 @@ def categorize():
                 mins.append(0)
                 maxs.append(0)
             index += 1
-        for row in data_rows[1:]:
+        for row in data_rows:
             index = 0
             for col in row:
                 if index > col_with_num:
@@ -41,7 +44,7 @@ def categorize():
                         if analyze(col) > maxs[index]:
                             maxs[index] = analyze(col)
                 index += 1
-        for row in data_rows[1:]:
+        for row in data_rows:
             index = 0
             cat_data_row = []
             for word in row:
@@ -52,7 +55,7 @@ def categorize():
                         level = 'None'
                     else:
                         level = math.floor((analyze(word)-col_min)/((col_max - col_min)/category_num))
-                    cat_data_row.append(data_rows[0][index] + '_' + str(level))
+                    cat_data_row.append(fields[index] + '_' + str(level))
                 else:
                     cat_data_row.append(word)
                 index += 1
@@ -62,7 +65,7 @@ def categorize():
 
 # compute the word collection
 def compute_word_set():
-    for row in cat_data_rows_list[1:]:
+    for row in cat_data_rows_list:
         for word in row:
             if word not in words_set:
                 words_set.add(word)
@@ -71,7 +74,7 @@ def compute_word_set():
 # compute the number of times when the subset appears in the rows
 def compute_frequency(subset):
     count = 0
-    for row in cat_data_rows_list[1:]:
+    for row in cat_data_rows_list:
         if subset.issubset(row):
             count += 1
     return count
@@ -87,7 +90,7 @@ def apriori(min_supp, min_conf):
 # compute the frequent item_set list has larger support than min_support
 # each element in the list is a list of length 2, list[0] is the set itself, list[1] is the support
 def compute_high_freq_set_list(min_supp):
-    min_row_num = (len(data_rows)-1)*min_supp
+    min_row_num = len(data_rows)*min_supp
     high_freq_set_list = []
     item_set = []
     high_freq_set_list_k = []
@@ -95,7 +98,7 @@ def compute_high_freq_set_list(min_supp):
         num = compute_frequency(set([word]))
         if num >= min_row_num:
             item_set.append(word)
-            high_freq_set_list_k.append([set([word]), float(num)/(len(data_rows)-1)])
+            high_freq_set_list_k.append([set([word]), float(num)/len(data_rows)])
     while len(high_freq_set_list_k) > 0:
         high_freq_set_list.extend(high_freq_set_list_k)
         high_freq_set_list_k_plus_1 = []
@@ -107,7 +110,7 @@ def compute_high_freq_set_list(min_supp):
                     new_s.add(item)
                     count = compute_frequency(new_s)
                     if count >= min_row_num and new_s not in high_freq_set_set_k_plus_1:
-                        high_freq_set_list_k_plus_1.append([new_s, float(count)/(len(data_rows)-1)])
+                        high_freq_set_list_k_plus_1.append([new_s, float(count)/len(data_rows)])
                         high_freq_set_set_k_plus_1.append(new_s)
         high_freq_set_list_k = high_freq_set_list_k_plus_1
     return high_freq_set_list
@@ -152,7 +155,7 @@ def compute_association_list_helper(ass_list, left_set, right_set):
 def compute_conf(association):
     left_count = 0
     right_count = 0
-    for row in cat_data_rows_list[1:]:
+    for row in cat_data_rows_list:
         if association[0].issubset(row):
             left_count += 1
             if association[1].issubset(row):
